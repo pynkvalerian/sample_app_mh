bb1ab262fd0c7418717862a3a347e83e013949c4
pynkvalerian <yenping.chew90@gmail.com>
2015-10-16 14:01:35 +0800

    chap3(static pages) refactored slightly.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,40 +3,35 @@ require 'rails_helper'
 RSpec.describe "StaticPages", type: :request do
 
   describe "Home page" do
-  	it "should have the content 'Sample App" do
+  	it "should have the right title with content 'Sample App" do
   		visit '/static_pages/home'
   		expect(page).to have_content('Sample App')
-  	end
-
-  	it "should have the right title" do
-  		visit '/static_pages/home'
   		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
   	end
   end
 
   describe "Help page" do
-  	it "should have the content 'Help" do
+  	it "should have the right title with  content 'Help" do
   		visit '/static_pages/help'
   		expect(page).to have_content('Help')
-  	end
-
-  	it "should have the right title" do
-  		visit '/static_pages/help'
   		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
   	end
   end
 
   describe "About page" do
-  	it "should have the content 'About us" do
+  	it "should have the right title with  content 'About us" do
   		visit '/static_pages/about'
   		expect(page).to have_content('About us')
-  	end
-
-  	it "should have the right title" do
-  		visit '/static_pages/about'
   		expect(page).to have_title("Ruby on Rails Tutorial Sample App | About us")
   	end
   end
 
 
+  describe "Contact page" do
+  	it "should have the right title with  content 'Contact us" do
+  		visit '/static_pages/contact'
+  		expect(page).to have_content('Contact us')
+  		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact us")
+  	end
+  end
 end
